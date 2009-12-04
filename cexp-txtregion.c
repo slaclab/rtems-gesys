b2efc99fd778b53be1912be848713df57f20a9fb
strauman <>
2009-12-04 18:33:01 +0000

    - provide BSP_sbrk_policy = -1; no more need for special PPC hacks...

--- cexp-txtregion.c
+++ cexp-txtregion.c
@@ -2,6 +2,10 @@
 #include <config.h>
 #endif
 
+#include <stdint.h>
+
+uint32_t BSP_sbrk_policy = -1;
+
 #ifdef RTEMS_CEXP_TEXT_REGION_SIZE
 /* hopefully goes into .bss */
 char cexpTextRegion[RTEMS_CEXP_TEXT_REGION_SIZE] = {0};
