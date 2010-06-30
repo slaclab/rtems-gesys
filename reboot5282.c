a8e53fe5c69229728e4bb3aed130efc8ff744a62
strauman <>
2010-06-30 14:39:20 +0000

    2010-06-30 (T.S.):
    	* reboot5282.c: More API mess. From some 4.10 pre-release on
    	bsp_reset() is declared in <bsp/bootcard.h>. As of 4.9.99 it is
    	bsp_reset(void) [more consistent], before it was bsp_reset(int).

--- reboot5282.c
+++ reboot5282.c
@@ -1,7 +1,19 @@
+#ifdef HAVE_CONFIG_H
+#include <config.h>
+#endif
+
 #include <bsp.h>
+
+#if RTEMS_VERSION_ATLEAST(4,9,88)
+#include <bsp/bootcard.h>
+#endif
 
 void
 rtemsReboot()
 {
+#if RTEMS_VERSION_ATLEAST(4,9,99)
+	bsp_reset();
+#else
 	bsp_reset(0);
+#endif
 }
