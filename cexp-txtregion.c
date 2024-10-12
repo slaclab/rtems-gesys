0a788d7da843b6564bed5b9fc24a44dc1675a771
Jeremy Lorelli <lorelli@slac.stanford.edu>
2024-10-11 18:30:45 -0700

    Add workaround for GCC 4.8.X bug with weak aliased symbols

--- cexp-txtregion.c
+++ cexp-txtregion.c
@@ -6,8 +6,13 @@
 
 uintptr_t BSP_sbrk_policy = -1; /*-1;*/
 
-#ifdef RTEMS_CEXP_TEXT_REGION_SIZE
+#if defined(RTEMS_CEXP_TEXT_REGION_SIZE) && RTEMS_CEXP_TEXT_REGION_SIZE > 0
 /* hopefully goes into .bss */
 char cexpTextRegion[RTEMS_CEXP_TEXT_REGION_SIZE] = {0};
 unsigned long cexpTextRegionSize = RTEMS_CEXP_TEXT_REGION_SIZE;
+#else
+/** JL: workaround for GCC 4.8.5 bug: we must define cexpTextRegionSize here, otherwise the weak alias cexpTextRegionSize in cexpsh will be used
+ * The symbol that cexpTextRegionSize is aliased to ends up in .rodata due to an optimization bug. This all works fine with -O0
+ */
+unsigned long cexpTextRegionSize = 0;
 #endif
